9758684ba94ac85dc7e0be3f23a97327d6127896
greg <bessong@gmail.com>
2025-06-30 22:06:59 +0200

    Add test file for GAA extension

---
+++ test-file.js
@@ -0,0 +1,11 @@
+// Test file for GAA extension
+function calculateTotal(price, tax) {
+    const total = price + tax;
+    return total;
+}
+
+function greetUser(name) {
+    console.log("Hello " + name);
+}
+
+module.exports = { calculateTotal, greetUser }; 
